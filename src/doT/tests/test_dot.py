f8e39eae53bacc492987400d8909336370c78cd7
lucemia <lucemia@gmail.com>
2023-05-03 17:30:31 +0800

    fix #4 add unit test

---
+++ src/doT/tests/test_dot.py
@@ -0,0 +1,7 @@
+from .. import template_settings, template
+
+def test_template(snapshot):
+    template_settings['strip'] = False
+
+    INPUT = r"{{?it.options == 1\n}}Option 1{{?? it.options == 2\n}}Option 2{{??\n}}Other option{{?}}:\n\n More text"
+    assert snapshot == template(INPUT, template_settings)
