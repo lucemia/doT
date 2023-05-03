411dae36372308daf4e8d00ee6433c3497ba159e
lucemia <lucemia@gmail.com>
2023-05-03 19:47:23 +0800

    add pre-commit

--- src/doT/tests/test_dot.py
+++ src/doT/tests/test_dot.py
@@ -1,7 +1,8 @@
-from .. import template_settings, template
+from .. import template, template_settings
+
 
 def test_template(snapshot):
-    template_settings['strip'] = False
+    template_settings["strip"] = False
 
     INPUT = r"{{?it.options == 1\n}}Option 1{{?? it.options == 2\n}}Option 2{{??\n}}Other option{{?}}:\n\n More text"
     assert snapshot == template(INPUT, template_settings)
